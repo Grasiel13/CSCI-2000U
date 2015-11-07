2f1e85eea9b2997631e9e51b7479323c22e2da8e
Sergio <sergio.majlufsuarez@uoit.net>
2015-11-07 11:08:02 -0500

    Assignment 2: completed strip.sh

--- Assignments/Assignment-2/has_no_e.py
+++ Assignments/Assignment-2/has_no_e.py
@@ -1,1 +1,11 @@
-has no e
+def has_no_e(line):
+    for i on line:
+        if (i == "e"):
+	    return False
+    return True
+reader = open("gadsby.txt",'r')
+
+for line in reader:
+    has_no_e(line)
+
+reader.close()
